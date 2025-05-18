3c985b0f8969a93947b25afece582bcfafabbd3a
mepix <13284555+mepix@users.noreply.github.com>
2025-05-18 18:14:43 +0000

    [Refactor] Create ROS2 Package
    Transformed the example code into a ROS2 package.  This enables us to
    start the simulated sensor using ros2 run and will make it convenient
    to launch multiple nodes.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'robotics_soln'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mepix',
+    maintainer_email='13284555+mepix@users.noreply.github.com',
+    description='3-DOF Sensor Package',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sensor = robotics_soln.sensor:main',
+            'example = robotics_soln.example:main',
+        ],
+    },
+)
