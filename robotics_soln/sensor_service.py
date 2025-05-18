12d29a18aa388a53910b6181224546d8f223482e
mepix <13284555+mepix@users.noreply.github.com>
2025-05-18 20:04:43 +0000

    [BROKEN] Custom messages doesn't really work

--- robotics_soln/sensor_service.py
+++ robotics_soln/sensor_service.py
@@ -7,6 +7,10 @@ import socket
 import numpy as np
 from std_msgs.msg import String
 
+# from robotics_soln.msg import SensorVec3D
+
+from robotics_soln.msg import SensorVec3D
+
 class SensorService(Node):
 
     def __init__(self):
